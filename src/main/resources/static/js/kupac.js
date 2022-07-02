60f4f68cb6c477759515ac1190b7a29c71d5c852
Nikola045 <80715268+Nikola045@users.noreply.github.com>
2022-07-02 20:07:39 +0200

    r

--- src/main/resources/static/js/kupac.js
+++ src/main/resources/static/js/kupac.js
@@ -1,3 +1,30 @@
+$(document).on("click","#kupacProfil",function (){
+
+    $.ajax({
+        type:"GET",
+        url:"http://localhost:8080/api/kupac/profil",
+        dataType:"json",
+        success:function (data){
+
+            var row = "<tr>";
+            row+="<td>" + data['username'] + "</td>";
+            row+="<td>" + data['password'] + "</td>";
+            row+="<td>" + data['ime'] + "</td>";
+            row+="<td>" + data['prezime'] + "</td>";
+            row+="<td>" + data['pol'] + "</td>";
+            row+="<td>" + data['datumRodjenja'] + "</td>";
+            row+="<td>" + data['uloga'] + "</td>";
+
+            $('#kupac').append(row);
+
+        },
+        error:function (data){
+            console.log("GRESKA:",data)
+        }
+    });
+});
+
+
 $(document).on("submit","#izmenaPodatakaZaKupca",function (event){
     event.preventDefault();
 
@@ -26,34 +53,6 @@ $(document).on("submit","#izmenaPodatakaZaKupca",function (event){
         }
     );
 });
-
-$(document).on("submit","#kupacProfil",function (event) {
-    event.preventDefault();
-    $.ajax({
-        type: "GET",
-        url: "http://localhost:8080/api/kupac/profil",
-        dataType: "json",
-        success: function (data) {
-            console.log("SUCCESS : ", data);
-                var row = "<tr>";
-                row += "<td>" + data[i]['username'] + "</td>";
-                row += "<td>" + data[i]['password'] + "</td>";
-                row += "<td>" + data[i]['ime'] + "</td>";
-                row += "<td>" + data[i]['prezime'] + "</td>";
-                row += "<td>" + data[i]['pol'] + "</td>";
-                row += "<td>" + data[i]['datumRodjenja'] + "</td>";
-                row += "<td>" + data[i]['uloga'] + "</td>";
-                row += "<td>" + data[i]['porudzbine'] + "</td>";
-                $('#kupac').append(row);
-
-        },
-        error: function (data) {
-            console.log("ERROR : ", data);
-        }
-    });
-});
-
-
 function formToJson1(user,pass,im,prez)
 {
     return JSON.stringify(
@@ -65,3 +64,124 @@ function formToJson1(user,pass,im,prez)
         }
     );
 }
+
+$(document).on("click","#pregledPorudzbina",function (){
+
+    $.ajax({
+        type:"GET",
+        url:"http://localhost:8080/api/kupac/pregled-porudzbina",
+        dataType:"json",
+        success:function (data){
+
+            var row = "<tr>";
+            row+="<td>" + data['vremePorudzbine'] + "</td>";
+            row+="<td>" + data['cena'] + "</td>";
+            row+="<td>" + data['trenutnoStanjePorudzbine'] + "</td>";
+
+            $('#porudzbine-kupac').append(row);
+
+        },
+        error:function (data){
+            console.log("GRESKA:",data)
+        }
+    });
+});
+
+$(document).on("submit","#dodajUKorpu",function (event){
+    event.preventDefault();
+
+    var nazivArtikla = $("#nazivArtikla").val();
+    var kolicina = $("#kolicina").val();
+    var nazivRestorana = $("#nazivRestorana").val();
+
+    var noviKorisnik = formToJson2(nazivArtikla,kolicina,nazivRestorana);
+
+    $.ajax(
+        {
+            type:"POST",
+            url:"http://localhost:8080/api/kupac/restoran/dodaj-u-korpu",
+            dataType:"json",
+            contentType:"application/json",
+            data:noviKorisnik,
+            success:function()
+            {
+                alert(nazivArtikla + " je uspesno dodato u kropu");
+            },
+            error:function (data)
+            {
+                alert("Greska prilikom dodavanja u korpu");
+            }
+        }
+    );
+});
+
+function formToJson2(naza,kol,nazr)
+{
+    return JSON.stringify(
+        {
+            "naizvArtikla":naza,
+            "kolicina":kol,
+            "nazivRestorana":nazr,
+        }
+    );
+}
+
+$(document).on("click","#pregledKorpe",function (){
+
+    $.ajax({
+        type:"GET",
+        url:"http://localhost:8080/api/kupac/pregled-korpe",
+        dataType:"json",
+        success:function (data){
+
+            var row = "<tr>";
+            row+="<td>" + data['idPorudzbine'] + "</td>";
+            row+="<td>" + data['kupac'] + "</td>";
+            row+="<td>" + data['poruceniartikli'] + "</td>";
+
+            $('#pregled_korpe').append(row);
+
+        },
+        error:function (data){
+            console.log("GRESKA:",data)
+        }
+    });
+});
+
+
+$(document).on("submit","#poruci",function (event){
+    event.preventDefault();
+
+    var nazivRestorana = $("#nazivArtikla").val();
+
+    var noviKorisnik = formToJson3(nazivRestorana);
+
+    $.ajax(
+        {
+            type:"POST",
+            url:"http://localhost:8080/api/kupac/poruci",
+            dataType:"json",
+            contentType:"application/json",
+            data:noviKorisnik,
+            success:function()
+            {
+                alert("Uspesno kreirana porudzbina iz "+nazivRestorana);
+            },
+            error:function (data)
+            {
+                alert("Greska prilikom dodavanja u korpu");
+            }
+        }
+    );
+});
+
+function formToJson3(nazr)
+{
+    return JSON.stringify(
+        {
+            "nazivRestorana":nazr
+                }
+    );
+}
+
+
